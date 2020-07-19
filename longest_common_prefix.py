09fff8d8c249364c84baeb85367437b14e30df1c
shahriar100 <shahriar.hasan100@gmail.com>
2020-07-20 02:26:25 +0600

    Day5 longest_common final

--- longest_common_prefix.py
+++ longest_common_prefix.py
@@ -4,25 +4,26 @@ class Solution(object):
         :type strs: List[str]
         :rtype: str
         """
+
         strs = sorted(strs, key = len)
 
         common = ''
-        for i, char in enumerate(strs[0]):
+        if len(strs) == 0:
+            return common
 
-            if all(char == word[i] for word in strs) == True:
-                common += char
-            else:
-                break
-        return common
-
-
+        else:
+            for i, char in enumerate(strs[0]):
+                if all(char == word[i] for word in strs) == True:
+                    common += char
+                else:
+                    break
+            return common
 
 
 
 if __name__ == '__main__':
 
     input = ["aa","a"]
-    # x = str("()[][]")
     instance = Solution()
     solution = instance.longestCommonPrefix(input)
     print(solution)
