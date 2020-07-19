c25945419f83581dc65c54d9bd571e5c3f4a8a23
shahriar100 <shahriar.hasan100@gmail.com>
2020-07-19 19:07:51 +0600

    Day_4 paranthesis stack solution

--- Day_4/valid_parentheses.py
+++ Day_4/valid_parentheses.py
@@ -1,8 +1,32 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        dict = {'{':'}',
-                '[':']',
-                '(':')' }
+
+        open_list = ["[","{","("]
+        close_list = ["]","}",")"]
+
+        # Function to check parentheses
+
+        stack = []
+        for i in s:
+            if i in open_list:
+                stack.append(i)
+            elif i in close_list:
+                pos = close_list.index(i)
+                if ((len(stack) > 0) and
+                    (open_list[pos] == stack[len(stack)-1])):
+                    stack.pop()
+                else:
+                    return False
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+
+
+
+        # dict = {'{':'}',
+        #         '[':']',
+        #         '(':')' }
 
         # odd_list = [s[j] for j in range(0,len(s),2)]+ [s[-1]]
         # print(odd_list)
@@ -23,7 +47,7 @@ class Solution:
 
 if __name__ == '__main__':
 
-    x = "([{[]}])"
+    x = "([{[]}]])"
     # x = str("()[][]")
     instance = Solution()
     solution = instance.isValid(x)
