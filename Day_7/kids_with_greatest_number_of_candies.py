a680fd37c3d3056fd581c50fd8250ff3d765c5fd
shahriar100 <shahriar.hasan100@gmail.com>
2020-07-21 10:17:20 +0600

    kids with greates candies faster

--- Day_7/kids_with_greatest_number_of_candies.py
+++ Day_7/kids_with_greatest_number_of_candies.py
@@ -2,3 +2,7 @@ class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         sorted_list = sorted(candies, reverse = True)
         return [candy + extraCandies >= sorted_list[0] for candy in candies]
+
+
+    def kidsWithCandies_Faster(self, candies: List[int], extraCandies: int) -> List[bool]:
+        return [candy + extraCandies >= max(candies) for candy in candies]
