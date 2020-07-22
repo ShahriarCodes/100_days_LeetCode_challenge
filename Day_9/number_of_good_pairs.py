632d37c56d14984278ee821b6c4d668a591f65c0
shahriar100 <shahriar.hasan100@gmail.com>
2020-07-23 01:23:31 +0600

    Day 9 number of good pairs hashmap

--- Day_9/number_of_good_pairs.py
+++ Day_9/number_of_good_pairs.py
@@ -11,4 +11,18 @@ class Solution(object):
                     count += 1
         return count
 
-        
+#using hashMap 
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+
+        hashMap = {}
+        res = 0
+
+        for number in nums:
+            if number in hashMap:
+                res += hashMap[number]
+                hashMap[number] += 1
+            else:
+                hashMap[number] = 1
+
+        return res
