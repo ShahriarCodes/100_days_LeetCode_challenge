5d28244165a1046cae137c2103d9a7ee0e5801f9
shahriar100 <shahriar.hasan100@gmail.com>
2020-07-24 21:45:47 +0600

    3rd commit

--- Day_11/shortest_unsorted_continuous_subarray.py
+++ Day_11/shortest_unsorted_continuous_subarray.py
@@ -4,11 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        lst = []
+        #check if ith num is greater than the following num
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i-1] :
+                lst.append(i)
+
+        if len(lst) > 0:
+            length = lst[-1] - lst[0] + 2
+        else:
+            length  = 0
+
+        return  length
 
 
 if __name__ == '__main__':
 
-    nums = [1,2,3,4]
+    nums = [2,6,4,8,10,9,9]
+    nums = [1,3,2,2,2]
+    nums = [4,2,2,2,1,9]
 
     instance = Solution()
     solution = instance.findUnsortedSubarray(nums)
