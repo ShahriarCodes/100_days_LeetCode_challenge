a2cc827ec771987767ac1421b59f12fd713496ad
shahriar100 <shahriar.hasan100@gmail.com>
2020-07-25 18:03:31 +0600

    Day 11 how many

---
+++ Day_11/how_many_numbers_smaller_than_the_current_number.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+
+
+
+if __name__ == '__main__':
+
+    nums = [2,6,4,8,10,9,9]
+    nums = [1,3,2,2,2]
+    nums = [4,2,2,2,1,9]
+
+    instance = Solution()
+    solution = instance.smallerNumbersThanCurrent(nums)
+    print(solution)
