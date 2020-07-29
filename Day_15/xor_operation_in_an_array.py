5284bf4809c147b2e84451286f5168b081cd8ef9
shahriar100 <shahriar.hasan100@gmail.com>
2020-07-29 21:54:00 +0600

    Day 15 final

--- Day_15/xor_operation_in_an_array.py
+++ Day_15/xor_operation_in_an_array.py
@@ -1,15 +1,20 @@
 class Solution:
     def xorOperation(self, n: int, start: int) -> int:
+        bitwise = 0
+        for i in range(n):
+            bitwise = bitwise ^ (start + 2*i)
+
+        return bitwise
 
 
 
 
 
-        
 
 if __name__ == '__main__':
 
-    input = "RLRRRLLRLL"
+    n = 10
+    start = 5
     instance = Solution()
-    solution = instance.balancedStringSplit(input)
+    solution = instance.xorOperation(n, start)
     print(solution)
