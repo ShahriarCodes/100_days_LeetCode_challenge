f22a5a39dd391426001cb6c6a9498d5879fad33c
shahriar100 <shahriar.hasan100@gmail.com>
2020-07-29 21:43:31 +0600

    Day 15 c2

---
+++ Day_15/xor_operation_in_an_array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def xorOperation(self, n: int, start: int) -> int:
+
+
+
+
+
+        
+
+if __name__ == '__main__':
+
+    input = "RLRRRLLRLL"
+    instance = Solution()
+    solution = instance.balancedStringSplit(input)
+    print(solution)
