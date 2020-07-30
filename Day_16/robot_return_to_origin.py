60d704e2d53c98bedb2f4dcae67b5a9271005e5f
shahriar100 <shahriar.hasan100@gmail.com>
2020-07-30 10:54:59 +0600

    Day 16 c2

--- Day_16/robot_return_to_origin.py
+++ Day_16/robot_return_to_origin.py
@@ -4,14 +4,27 @@ class Solution(object):
         :type moves: str
         :rtype: bool
         """
+        x_axis = {'R':1, 'L':-1}
+        y_axis = {'U':1, 'D':-1,}
+
+        position = [0,0]
+        for move in moves:
+            if move in x_axis:
+                position[0] += x_axis[move]
+            if move in y_axis:
+                position[1] += y_axis[move]
+        print(position)
+        if position == [0,0]:
+            return True
+        else:
+            return False
 
 
 
 
 if __name__ == '__main__':
 
-    n = 10
-    start = 5
+    input = "ULRD"
     instance = Solution()
-    solution = instance.judgeCircle(n, start)
+    solution = instance.judgeCircle(input)
     print(solution)
