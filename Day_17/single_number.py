c280ae0b1b76d17598f8eed80934e89306b65b98
shahriar100 <shahriar.hasan100@gmail.com>
2020-07-31 12:33:35 +0600

    Day 17 c3

--- Day_17/single_number.py
+++ Day_17/single_number.py
@@ -4,11 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        for i, num in enumerate(nums):
+            if i == 0 and num not in nums[i+1:]:
+                return num
+
+            elif num not in nums[i+1:] and i != 0 and num not in nums[:i]:
+                return num
+
+            elif i == len(nums) and num not in nums[:i]:
+                return num
+
+    def singleNumber_one_liner(self, nums):
+        return 2 * sum(set(nums)) - sum(nums)
 
 
 if __name__ == '__main__':
 
-    input = "RLRRRLLRLL"
+    input = [2,2,1]
     instance = Solution()
-    solution = instance.singleNumber(input)
+    %timeit solution = instance.singleNumber(input)
+    %timeit solution2 = instance.singleNumber_one_liner(input)
+
     print(solution)
+
+[4,1,2,1,2][0+1:]
+[4,1,2,1,2][:1]
