7f276690441aef567602df9c9beb7a32a096b03d
shahriar100 <shahriar.hasan100@gmail.com>
2020-07-31 10:43:38 +0600

    day 17 c2

---
+++ Day_17/single_number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+
+if __name__ == '__main__':
+
+    input = "RLRRRLLRLL"
+    instance = Solution()
+    solution = instance.singleNumber(input)
+    print(solution)
