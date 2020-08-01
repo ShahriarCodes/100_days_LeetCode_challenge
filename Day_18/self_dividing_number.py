d6c748c8395bb2963dc86f02f6884b5b4a7dff0e
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-01 08:29:41 +0600

    Day 18 c 2

---
+++ Day_18/self_dividing_number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def selfDividingNumbers(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: List[int]
+        """
+
+
+if __name__ == '__main__':
+
+    left = 1
+    right = 22
+    instance = Solution()
+    solution = instance.selfDividingNumbers(left, right)
+
+    print(solution)
