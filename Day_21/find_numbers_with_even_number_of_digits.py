a6d4ec30c4cce4914eee2d8112d347e6e531b8ee
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-04 08:21:29 +0600

    Day 21 c2

---
+++ Day_21/find_numbers_with_even_number_of_digits.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def findNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+
+
+
+
+if __name__ == '__main__':
+
+    nums = [1,2,3,4,0]
+    index = [0,1,2,3,0]
+    instance = Solution()
+    solution = instance.createTargetArray(nums, index)
+
+    print(solution)
