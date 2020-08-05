b9119acd65daabeb14db1645805a7cb196ae071e
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-05 08:58:56 +0600

    Day 22

---
+++ Day_22/remove_outermost_parenthesis.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeOuterParentheses(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+
+
+
+if __name__ == '__main__':
+
+    nums = [555,901,482,1771]
+    instance = Solution()
+    solution = instance.removeOuterParentheses(nums)
+
+    print(solution)
