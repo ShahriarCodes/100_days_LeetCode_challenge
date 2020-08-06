bfefdb4c077600cfb26c344038efebdd86ed51ad
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-06 15:24:40 +0600

    Day 23 c

--- Day_23/reverse_words_in_a_string_III.py
+++ Day_23/reverse_words_in_a_string_III.py
@@ -1,13 +1,26 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
+        s = s[::-1]
+        s = s.split(' ')
+
+        final = ''
+        i = len(s) - 1
+        while i >= 0:
+            final += s[i] + ' '
+            i -= 1
+        final = final [:-1]
+        return final
+
+
 
 
 
 
 if __name__ == '__main__':
 
-    nums = "()()"
+    nums = "Let's take LeetCode contest"
     instance = Solution()
     solution = instance.reverseWords(nums)
 
     print(solution)
+"Let's take LeetCode contest".pop()
