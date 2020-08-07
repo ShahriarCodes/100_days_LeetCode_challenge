e2a4b69e72febb4fcd4bd40b9e8ee99d748ddbdd
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-07 09:16:53 +0600

    Day 24 minimum time c2

---
+++ Day_24/minimum_time_visiting_all_points.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minTimeToVisitAllPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+
+
+
+        
+if __name__ == '__main__':
+
+    nums = "Let's take LeetCode contest"
+    instance = Solution()
+    solution = instance.reverseWords(nums)
+
+    print(solution)
