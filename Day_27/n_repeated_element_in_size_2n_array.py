3fb16259df66ae2d3287e8a296637c3b8101b04c
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-10 08:30:04 +0600

    Day 27 n repeated element in 2n array c3

--- Day_27/n_repeated_element_in_size_2n_array.py
+++ Day_27/n_repeated_element_in_size_2n_array.py
@@ -1,3 +1,19 @@
+class Solution(object):
+    def repeatedNTimes(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        dict = {}
+
+        for num in A:
+            if num not in dict:
+                dict[num] = 1
+            else:
+                dict[num] += 1
+                return num
+
+
 if __name__ == '__main__':
 
     N = 8
