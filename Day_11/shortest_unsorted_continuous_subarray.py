e339fcd262530709131450d73df4fd384cf7fa2d
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-10 09:28:30 +0600

    Day 11 shortest unsorted continuous array

--- Day_11/shortest_unsorted_continuous_subarray.py
+++ Day_11/shortest_unsorted_continuous_subarray.py
@@ -4,28 +4,33 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        lst = []
-        index = []
-        #check if ith num is greater than the following num
-        for i in range(1, len(nums)):
-            if nums[i] < nums[i-1] :
-                lst.append(nums[i])
-                index.append(i)
-
-        # if len(lst) > 0:
-        #     length = lst[-1] - lst[0] + 2
-        # else:
-        #     length  = 0
-
-        return  lst, index
-
+        # lst = []
+        # index = []
+        # #check if ith num is greater than the following num
+        # for i in range(1, len(nums)):
+        #     if nums[i] < nums[i-1] :
+        #         lst.append(nums[i])
+        #         index.append(i)
+        #
+        # # if len(lst) > 0:
+        # #     length = lst[-1] - lst[0] + 2
+        # # else:
+        # #     length  = 0
+        #
+        # return  lst, index
+        count_dict = {}
+        for num in nums:
+            if num not count_dict:
+                count_dict[num] = 1
+            else:
+                count_dict[num] += 1
 
 if __name__ == '__main__':
 
     nums = [2,6,4,8,10,9,9]
     nums = [1,3,2,2,2]
-    nums = [4,2,2,2,1,9]
-
+    nums = [4,2,2,2,3,1,9]
+    nums = [1,2,3,3,3]
     instance = Solution()
     solution = instance.findUnsortedSubarray(nums)
     print(solution)
