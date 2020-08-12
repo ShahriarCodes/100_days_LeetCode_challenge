8608965abda9dd3d39917c175074616421aeea36
Shahriar Hasan <45923195+shahriar100@users.noreply.github.com>
2020-08-12 07:00:14 +0600

    Day 28 c2

---
+++ Day_28/peak_index_in_a_mountain_array.py
@@ -0,0 +1,16 @@
+
+class Solution(object):
+
+    def peakIndexInMountainArray(self, A):
+
+        """
+
+        :type A: List[int]
+
+        :rtype: int
+
+        """
+
+        return A.index(max(A))
+
+​
