da44439d4f04c797aaf3bb346e3a9f6f3f8b975b
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-16 13:26:04 +0600

    Day 33 c2

---
+++ Day_33/island_perimeter.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def islandPerimeter(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+
+
+
+
+if __name__ == '__main__':
+
+    A = [[0,1,0,0],
+        [1,1,1,0],
+        [0,1,0,0],
+        [1,1,0,0]]
+    instance = Solution()
+    solution = instance.islandPerimeter(A)
+
+    print(solution)
