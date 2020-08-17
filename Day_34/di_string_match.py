5b8079ae0f20cf9c86d2a977027805955d763658
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-17 10:39:02 +0600

    Day_34 DI string match c3

--- Day_34/di_string_match.py
+++ Day_34/di_string_match.py
@@ -4,6 +4,17 @@ class Solution(object):
         :type S: str
         :rtype: List[int]
         """
+        if S[0] == 'I':
+            A = [0]
+        elif S[0] == 'D':
+            A = [len(S)]
+
+        for i, s in enumerate(S):
+            if s == 'I':
+                A.append(A[i]+1)
+            if s == 'D':
+                A.append(A[i]-1)
+        return A
 
 
 
@@ -11,11 +22,8 @@ class Solution(object):
 
 if __name__ == '__main__':
 
-    A = [[0,1,0,0],
-        [1,1,1,0],
-        [0,1,0,0],
-        [1,1,0,0]]
+    S = "IDID"
     instance = Solution()
-    solution = instance.diStringMatch(A)
+    solution = instance.diStringMatch(S)
 
     print(solution)
