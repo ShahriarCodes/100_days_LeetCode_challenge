a9811bcaa3ba18c5ffb190d935be92d234034373
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-17 10:12:54 +0600

    Day_34 DI string match c2

---
+++ Day_34/di_string_match.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def diStringMatch(self, S):
+        """
+        :type S: str
+        :rtype: List[int]
+        """
+
+
+
+
+
+if __name__ == '__main__':
+
+    A = [[0,1,0,0],
+        [1,1,1,0],
+        [0,1,0,0],
+        [1,1,0,0]]
+    instance = Solution()
+    solution = instance.diStringMatch(A)
+
+    print(solution)
