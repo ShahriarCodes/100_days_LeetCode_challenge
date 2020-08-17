5e63084d9866eac52a48937af7d4221551f650be
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-17 10:48:00 +0600

    Day_34 DI string match final

--- Day_34/di_string_match.py
+++ Day_34/di_string_match.py
@@ -10,14 +10,11 @@ class Solution(object):
         for s in S:
             if s == 'I':
                 A.append(low)
-                low += 1 
+                low += 1
             elif s == 'D':
                 A.append(high)
                 high -= 1
         return A + [low]
-
-
-
 
 
 if __name__ == '__main__':
