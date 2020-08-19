3af961b757c0a83deb7a39807e96d28999fb2129
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-19 16:39:09 +0600

    Day 36 linked list -- convert binary

---
+++ Day_36/convert_binary_number_in_a_linked_list_to_integer.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def getDecimalValue(self, head):
+        """
+        :type head: ListNode
+        :rtype: int
+        """
+        itr = head
+        llstr = ''
+        while itr:
+
+            itr = itr.next
+        print(llstr)
