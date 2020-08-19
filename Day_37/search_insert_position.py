ba2e44bfff93724fb7cfe86d489c9fa99844f3ba
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-20 00:29:19 +0600

    Day_37 search insert position final

--- Day_37/search_insert_position.py
+++ Day_37/search_insert_position.py
@@ -5,12 +5,22 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
+        len_num = len(nums)
+        for i, num in enumerate(nums):
+            if nums[i] > target:
+                return i
+            if num == target:
+                return i
+
+        if target > nums[len_num-1]:
+            return len_num
+            
 
 
 if __name__ == '__main__':
 
     nums = [1,3,5,6]
-    target = 5
+    target = 7
     instance = Solution()
     solution = instance.searchInsert(nums, target)
 
