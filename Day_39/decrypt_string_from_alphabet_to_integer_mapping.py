e3a028dd505d3aab0b9cdb3ca8818905c5675964
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-22 09:29:22 +0600

    Day_39/decrypt_string_from_alphabet_to_integer_mapping.py

---
+++ Day_39/decrypt_string_from_alphabet_to_integer_mapping.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def freqAlphabets(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+
+if __name__ == '__main__':
+
+    s = "10#11#12"
+    instance = Solution()
+    solution = instance.freqAlphabets(s)
+
+    print(solution)
