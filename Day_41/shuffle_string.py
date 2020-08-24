978d34a4c9150e995f51dd04e76b0d91897fa3f5
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-24 08:29:45 +0600

    Day41 shuffle string c2

---
+++ Day_41/shuffle_string.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def restoreString(self, s, indices):
+        """
+        :type s: str
+        :type indices: List
+        :rtype: str
+        """
+
+
+
+
+if __name__ == '__main__':
+
+    s = 'coldeleet'
+    indices = [4,5,6,7,0,2,3,1]
+    instance = Solution()
+    solution = instance.restoreString(s, indices)
+
+    print(solution)
