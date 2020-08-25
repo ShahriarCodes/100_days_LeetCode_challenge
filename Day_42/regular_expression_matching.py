dcbfd8ae7f0520cca46908137cd5e99699ccbf9a
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-25 19:33:34 +0600

    Day 42 Regular Expression matching c2

---
+++ Day_42/regular_expression_matching.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+
+
+
+
+if __name__ == '__main__':
+
+    s = "aa"
+    p = "a*"
+    instance = Solution()
+    solution = instance.isMatch(s, p)
+
+    print(solution)
