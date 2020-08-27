fd1d60c4a76f555e3394fb0120424d2bfa00ccdf
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-27 13:31:55 +0600

    Day 44 replace elements

--- Day_44/replace_elements_with_greatest_element_on_right_side.py
+++ Day_44/replace_elements_with_greatest_element_on_right_side.py
@@ -18,6 +18,16 @@ class Solution(object):
 
         return new_arr
 
+#faster
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        maxSoFar = arr[-1]
+        arr[-1] = -1
+        for i in range(len(arr) - 2, -1, -1):
+            temp = arr[i]
+            arr[i] = maxSoFar
+            maxSoFar = max(maxSoFar, temp)
+        return arr
 
 if __name__ == '__main__':
 
