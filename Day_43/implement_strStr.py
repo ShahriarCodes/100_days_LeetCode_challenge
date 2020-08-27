81bf18b8210fb1aa02649a796e79dc2123f3bfad
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-27 11:09:30 +0600

    Day 44 intersection c2

--- Day_43/implement_strStr.py
+++ Day_43/implement_strStr.py
@@ -5,12 +5,16 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-
+        subset = []
+        for num in nums1:
+            if num in nums2 and num not in subset:
+                subset.append(num)
+        return subset
 
 if __name__ == '__main__':
 
-    nums1 = [1,2,2,1]
-    nums2 = [2,2]
+    nums1 = [4,9,5]
+    nums2 = [9,4,9,8,4]
     instance = Solution()
     solution = instance.intersection(nums1, nums2)
 
