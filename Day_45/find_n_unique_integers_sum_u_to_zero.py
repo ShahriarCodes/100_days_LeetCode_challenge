654e0e096fd5b4e265ce919c775a996d9930dab9
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-28 11:10:27 +0600

    Day 45 n unique integers

---
+++ Day_45/find_n_unique_integers_sum_u_to_zero.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def sumZero(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        lst = []
+        #for even n
+        if n%2 == 0:
+            for i in range(1,int(n/2 + 1)):
+                lst.append(i)
+                lst.append(-i)
+        elif n%2 != 0:
+            for i in range(1, int(n/2 + 1)):
+                lst.append(i)
+                lst.append(-i)
+            lst.append(0)
+        return lst
+
+
+
+if __name__ == '__main__':
+    n = 7
+    instance = Solution()
+    solution = instance.sumZero(n)
+
+    print(solution)
+    
