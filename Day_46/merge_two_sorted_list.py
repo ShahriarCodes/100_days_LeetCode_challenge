d4281ea8871c80920c36f0a86ae9bc44f8a0a98a
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-29 23:41:33 +0600

    Day 44 merge linked list unfinished

---
+++ Day_46/merge_two_sorted_list.py
@@ -0,0 +1,61 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        itr = self.head
+        llstr = ''
+        while itr:
+            llstr += str(itr.data) + ' --> '
+            itr = itr.next
+        print(llstr)
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        # last_node = l1
+        # while last_node.next:
+        #     last_node = last_node.next
+        # last_node.next = l2
+
+        llist1 = LinkedList()
+        llist1.append("A")
+        llist1.append('B')
+
+
+        return llist1
+if __name__ == '__main__':
+
+
+    instance = Solution()
+    solution = instance.mergeTwoLists(1,2)
+    print(solution)
