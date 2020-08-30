031886ffe99fa67be1b508468b5a7eb6274f293f
shahriar100 <shahriar.hasan100@gmail.com>
2020-08-30 15:34:09 +0600

    Day 46 merge two sorted lsit

--- Day_46/merge_two_sorted_list.py
+++ Day_46/merge_two_sorted_list.py
@@ -1,61 +1,26 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy = ListNode()
+        cur = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        
+        if l1:
+            cur.next = l1
+        elif l2:
+            cur.next = l2
+        
+        return dummy.next
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-
-    def print_list(self):
-        if self.head is None:
-            print("Linked list is empty")
-            return
-        itr = self.head
-        llstr = ''
-        while itr:
-            llstr += str(itr.data) + ' --> '
-            itr = itr.next
-        print(llstr)
-
-    def append(self, data):
-        new_node = Node(data)
-
-        if self.head is None:
-            self.head = new_node
-            return
-
-        last_node = self.head
-        while last_node.next:
-            last_node = last_node.next
-        last_node.next = new_node
-
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        # last_node = l1
-        # while last_node.next:
-        #     last_node = last_node.next
-        # last_node.next = l2
-
-        llist1 = LinkedList()
-        llist1.append("A")
-        llist1.append('B')
-
-
-        return llist1
-if __name__ == '__main__':
-
-
-    instance = Solution()
-    solution = instance.mergeTwoLists(1,2)
-    print(solution)
