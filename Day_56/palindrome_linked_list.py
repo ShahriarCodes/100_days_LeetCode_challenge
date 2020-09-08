5e9bfe05a741de9430b105fefaee32bea9ceb64b
shahriar100 <shahriar.hasan100@gmail.com>
2020-09-08 16:22:20 +0600

    Day 56 palindrome linked list

--- Day_56/palindrome_linked_list.py
+++ Day_56/palindrome_linked_list.py
@@ -22,6 +22,8 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
+        # dummy = ListNode(-1)
+        # dummy.next = head
         fast = head
         slow = head
         node = head
@@ -31,25 +33,35 @@ class Solution(object):
             slow = slow.next
             fast = fast.next.next
             acc += 1
-            if fast.next == None and fast != None:
-                # node.next = None
+            
+            #odd case
+            if fast.next == None:
+                case = 1
                 break
-            if fast ==
+                
+            #even case
+            if fast.next.next == None:
+                case =  0
+                break
         
-        slow_reversed = self.reverseList(slow)
-        
+        if case == 1:
+            reverse = self.reverseList(slow) #odd
+        if case == 0: 
+            reverse = self.reverseList(fast) #even
+            
         print('head ::  ', head)
+        print('fast ::  ', fast)
         print('slow :: ', slow)      
-        print('slow_reversed :: ', slow_reversed)
+        print('reverse :: ', reverse)
 
         new_acc = 0
         while True:
             new_acc +=1
                 
-            if new_acc <= acc:
-                if head.val == slow_reversed.val:
+            if new_acc < acc:
+                if head.val == reverse.val:
                     head = head.next
-                    slow_reversed = slow_reversed.next
+                    reverse = reverse.next
                     
                 else:
                     return False
