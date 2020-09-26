cc01f37bb1084a90e2b4a4443479c89f7d65e59b
shahriar100 <shahriar.hasan100@gmail.com>
2020-09-26 11:48:55 +0600

    Day 75 Maximum depth of binary tree

---
+++ Day_75/maximum_depth_of_binary_tree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+
+        depth = 0
+        queue = [root]
+        next_queue = []
+        level = []
+        result = []
+        while queue != []:
+            for root in queue:
+                level.append(root.val)
+                print('inner loop level',level)                
+                
+                if root.left is not None:
+                    next_queue.append(root.left)
+                if root.right is not None:
+                    next_queue.append(root.right)
+            depth += 1
+            print('outer loop level',level)
+            result.append(sum(level)/len(level))
+            level = []
+            queue = next_queue
+            next_queue = []
+        return depth    
