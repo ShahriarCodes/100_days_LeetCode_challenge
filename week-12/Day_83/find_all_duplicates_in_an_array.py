5cf1d4ecdaa4d2e2cdd5fb06b50581033e199c40
shahriar100 <shahriar.hasan100@gmail.com>
2020-10-04 08:49:39 +0600

    Day 83 find all duplicates -- added naive solution O(1)space, O(nlogn) runtime

--- week-12/Day_83/find_all_duplicates_in_an_array.py
+++ week-12/Day_83/find_all_duplicates_in_an_array.py
@@ -13,3 +13,18 @@ class Solution(object):
             nums[index] = - nums[index]
                 
         return output
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        if len(nums) < 2:
+            return []
+
+        nums = sorted(nums)
+        prev_num = nums[0]
+        output = []
+        for i in range(1, len(nums)):
+            if nums[i] == prev_num:
+                output.append(nums[i])
+            elif nums[i] != prev_num:
+                prev_num = nums[i]
+        return output
