147c3330766506f866cb9603f413d10e9f2a6a5d
shahriar100 <shahriar.hasan100@gmail.com>
2020-10-05 13:03:04 +0600

    Day 84 set matrix to zeroes --optimized solution O(m*n) time, O(1) space

--- week-12/Day_84/set_matrix_zeroes.py
+++ week-12/Day_84/set_matrix_zeroes.py
@@ -23,3 +23,31 @@ class Solution:
                 matrix[i][j] = 0
         print(matrix)
         print(i_placeholder, j_placeholder)
+
+
+# space optimized
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        row = len(matrix)
+        col = len(matrix[0])
+        col_0 = True
+        
+        for i in  range(row):
+            if matrix[i][0] == 0 : col_0 = False
+            for j in range(1,col):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        
+        for i in reversed(range(row)):
+            for j in reversed(range(1,col)):
+                if matrix[0][j] == 0 or matrix[i][0] == 0:
+                    matrix[i][j] = 0
+            if col_0 == False: matrix[i][0] = 0
+        
+        print(matrix)
+
